86e8b2411f8e1a5a30c9d1d1ac4e2743e4d020b0
Dmtrii Vinogradov <dmitriy.vinogradov@eltex.loc>
2026-04-04 00:33:28 +0700

    feat: revert changes of old impl

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -75,8 +75,8 @@ export async function listDirectories(dirPath: string): Promise<string[]> {
   try {
     const entries = await fs.readdir(dirPath, { withFileTypes: true });
     return entries
-        .filter(entry => entry.isDirectory())
-        .map(entry => entry.name);
+      .filter(entry => entry.isDirectory())
+      .map(entry => entry.name);
   } catch {
     return [];
   }
